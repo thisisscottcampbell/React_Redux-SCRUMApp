043df2612eb8a16034fa79d2b4e0f2bfa0808e13
Scott Campbell <thisisscottcampbell@gmail.com>
2021-06-21 19:43:23 -0400

    edit modal

--- scrum-app/src/App.js
+++ scrum-app/src/App.js
@@ -3,6 +3,7 @@ import SearchBar from './components/layout/SearchBar';
 import Logs from './components/logs/Logs';
 import AddButton from './components/layout/AddButton';
 import AddLogModal from './components/logs/AddLogModal';
+import EditLogModal from './components/logs/EditLogModal';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 
@@ -17,6 +18,7 @@ const App = () => {
 			<div className="container">
 				<AddButton />
 				<AddLogModal />
+				<EditLogModal />
 				<Logs />
 			</div>
 		</div>
