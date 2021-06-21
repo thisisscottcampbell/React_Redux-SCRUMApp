9d94adaf54b9b2379f077d83f39032678503f9f0
Scott Campbell <thisisscottcampbell@gmail.com>
2021-06-21 19:38:58 -0400

    add modal

--- scrum-app/src/App.js
+++ scrum-app/src/App.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
 import SearchBar from './components/layout/SearchBar';
 import Logs from './components/logs/Logs';
+import AddButton from './components/layout/AddButton';
+import AddLogModal from './components/logs/AddLogModal';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 
@@ -13,6 +15,8 @@ const App = () => {
 		<div>
 			<SearchBar />
 			<div className="container">
+				<AddButton />
+				<AddLogModal />
 				<Logs />
 			</div>
 		</div>
